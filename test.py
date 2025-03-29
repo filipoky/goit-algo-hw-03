6342ee09089cc7206f1b769bb86b264fa800b920
Yuriy Filipkov <yuriyfilipkov@gmail.com>
2025-03-30 00:03:54 +0100

    4th commit

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-print("Hello World")
+# print("Hello World")
 
 
 # from datetime import datetime, date, timedelta
@@ -25,28 +25,41 @@ from datetime import datetime
 
 def get_days_from_today(date):
     try:
-        # Перетворюємо рядкове представлення дати в об'єкт datetime
-        input_date = datetime.strptime(date, "%Y-%m-%d").date()
-        
-        # Отримуємо поточну дату
-        today = datetime.today().date()
-        
-        # Розраховуємо різницю в днях
-        # difference = (today - input_date).days
-        difference = today.toordinal() - input_date.toordinal()
-        
+        input_date = datetime.strptime(date, "%Y-%m-%d").date()    # Перетворюємо рядкове представлення дати в об'єкт datetime
+        today = datetime.today().date()                            # Отримуємо поточну дату
+        difference = today.toordinal() - input_date.toordinal()    # Розраховуємо різницю в днях
         return difference
     
     except ValueError:
         # Якщо формат дати некоректний, повертаємо помилку
         return "Невірний формат дати. Будь ласка, використовуйте формат 'YYYY-MM-DD'."
 
-# Тест
+# Тести
 print(get_days_from_today("2026-10-09"))  # Приклад з датою в майбутньому
 print(get_days_from_today("2022-02-24"))  # Приклад з датою в минулому
 print(get_days_from_today("invalid-date"))  # Приклад з невірним форматом
 
 
+
+
+
+
+
+
+# from datetime import datetime
+
+# def get_days_from_today(date):
+#     try:
+#         input_date = datetime.strptime(date, "%Y-%m-%d").date()
+#         today = datetime.today().date()
+#         return (today - input_date).days  
+#     except ValueError:
+#         return "Невірний формат дати. Будь ласка, використовуйте формат 'YYYY-MM-DD'."
+
+# # Тести
+# print(get_days_from_today("2026-10-09"))  
+# print(get_days_from_today("2022-02-24"))  
+# print(get_days_from_today("invalid-date"))  
 
 
 
